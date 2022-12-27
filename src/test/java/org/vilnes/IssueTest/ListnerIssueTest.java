97ec5cb7b226cea2bd44ae6165fa70f9202e251b
Dmitry Rodichev <ya.vilnes@yandex.ru>
2022-12-27 16:55:22 +0700

    first commit

---
+++ src/test/java/org/vilnes/IssueTest/ListnerIssueTest.java
@@ -0,0 +1,64 @@
+package org.vilnes.IssueTest;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+
+public class ListnerIssueTest extends TestBase {
+    private static final String GITHUB_URL = "https://github.com";
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final String ISSUE_NUMBER = "#81";
+    @Test
+    @DisplayName("Проверка наличия Issue с наименованием {issue}")
+    @Feature("Отображение номера Issues")
+    @Story("Проверка наличия Issue с наименованием {issue}")
+    @Owner("Dmitry Rodichev")
+    @Severity(SeverityLevel.NORMAL)
+    void testIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу" + GITHUB_URL, () -> {
+            open(GITHUB_URL);
+        });
+        step("Ищем репозиторий" + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").setValue(REPOSITORY).submit();
+        });
+        step("Переходим в репозиторий" + REPOSITORY, () -> {
+            $(By.linkText(REPOSITORY)).click();
+        });
+        step("Переходим в раздел Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером" + ISSUE_NUMBER, () -> {
+            $(withText(ISSUE_NUMBER)).should(Condition.exist);
+        });
+    }
+        @Test
+        @DisplayName("Проверка наличия Issue с наименованием {issue}")
+        @Feature("Отображение номера Issues")
+        @Story("Проверка наличия Issue с наименованием {issue}")
+        @Owner("Dmitry Rodichev")
+        @Severity(SeverityLevel.NORMAL)
+        public void stepTestIssue() {
+            SelenideLogger.addListener("allure", new AllureSelenide());
+            StepTestIssue StepTestIssue = new StepTestIssue();
+
+            StepTestIssue.openStartPage(GITHUB_URL);
+            StepTestIssue.searchRepo(REPOSITORY);
+            StepTestIssue.openRepo(REPOSITORY);
+            StepTestIssue.openIssueTab();
+            StepTestIssue.checkIssueByName(ISSUE_NUMBER);
+
+        }
+    }
+
